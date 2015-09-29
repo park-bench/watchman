42447c41093088acc5f866fa79f4cd3fd46f25b5
eviljoel <eviljoel@linux.com>
2015-09-28 21:40:49 -0500

    Added config file support.

--- src/opt/clover/cloverconfig.py
+++ src/opt/clover/cloverconfig.py
@@ -7,9 +7,11 @@ class CloverConfig:
     # Reads the clover subprocess configuration and exits the program there is an error.
     def __init__(self, config_file):
 
-        self.logger = timber.get_instance()
+        logger = timber.get_instance()
 
         logger.info('Validating subprocess configuration.')
+
+        config_helper = confighelper.ConfigHelper()
 
         # By the time this method is called in the subprocess, the logging should already be started. 
         #   However, the main process still needs to validate the subprocess's logging parameters.
@@ -19,7 +21,7 @@ class CloverConfig:
 
         self.save_path = config_helper.verify_string_exists(config_file, 'image_save_path')
         # Time in seconds
-        self.movement_time_threshold = config_helper.verify_number_exists(config_file, '')
+        self.movement_time_threshold = config_helper.verify_number_exists(config_file, 'movement_time_threshold')
         # Can be increased to make movements less sensitive
         self.prior_movements_per_threshold = config_helper.verify_integer_exists(config_file, 'prior_movements_per_threshold')
         # How much the two frames have to vary to be considered different
@@ -30,7 +32,7 @@ class CloverConfig:
         self.third_email_image_save_times = config_helper.verify_number_list_exists(config_file, 'third_email_image_save_times')
         # Time in seconds since last e-mail
         self.third_email_delay = config_helper.verify_number_exists(config_file, 'third_email_delay')
-        sefl.subsequent_email_image_save_times = config_helper.verify_number_list_exists(config_file, 'subsequent_email_image_save_times')
+        self.subsequent_email_image_save_times = config_helper.verify_number_list_exists(config_file, 'subsequent_email_image_save_times')
         # Time in seconds since last e-mail
         self.subsequent_image_delay = config_helper.verify_number_exists(config_file, 'subsequent_image_delay')
         # Time in seconds since last e-mail triggering motion
