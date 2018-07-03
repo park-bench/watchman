159037afffe814a0b214bf14f493f218f36f7138
eviljoel <eviljoel@linux.com>
2018-07-02 20:35:18 -0500

    Added safety checks for configuration file permissions.

--- src/usr/share/watchman/watchman-daemon.py
+++ src/usr/share/watchman/watchman-daemon.py
@@ -53,6 +53,12 @@ VIDEO_DEVICE_PREFIX = '/dev/video%d'
 PROGRAM_UMASK = 0o027  # -rw-r----- and drwxr-x---
 
 
+class InitializationException(Exception):
+    """Indicates an expected fatal error occurred during program initialization.
+    Initialization is implied to mean, before daemonization.
+    """
+
+
 def get_user_and_group_ids():
     """Get user and group information for dropping privileges.
 
@@ -117,6 +123,22 @@ def read_configuration_and_create_logger(program_uid, program_gid):
     config = watchmanconfig.WatchmanConfig(config_parser)
 
     return (config, config_helper, logger)
+
+
+# TODO: Consider checking ACLs. (gpgmailer issue 22)
+def verify_safe_file_permissions():
+    """Crashes the application if unsafe file permissions exist on application configuration
+    files.
+    """
+    # The configuration file should be owned by root.
+    config_file_stat = os.stat(CONFIGURATION_PATHNAME)
+    if config_file_stat.st_uid != 0:
+        raise InitializationException(
+            'File %s must be owned by root.' % CONFIGURATION_PATHNAME)
+    if bool(config_file_stat.st_mode & (stat.S_IROTH | stat.S_IWOTH | stat.S_IXOTH)):
+        raise InitializationException(
+            "File %s cannot have 'other user' access permissions set."
+            % CONFIGURATION_PATHNAME)
 
 
 def create_directory(system_path, program_dirs, uid, gid, mode):
@@ -246,7 +268,9 @@ config, config_helper, logger = read_configuration_and_create_logger(
 
 watchman_subprocess = None
 try:
-    # Restore root permissions.
+    verify_safe_file_permissions()
+
+    # Re-establish root permissions to create required directories.
     os.seteuid(os.getuid())
     os.setegid(os.getgid())
 
