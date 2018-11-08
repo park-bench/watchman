dc903adba5b1fcb9bbd507a9e79ece50df823c00
Brittney K Scaccia <bkscaccia@gmail.com>
2018-11-07 18:48:42 -0600

    Relinquishing copyright to Joel Allen Luellwitz and Emily Klapp.

--- src/usr/share/watchman/watchman-subprocess.py
+++ src/usr/share/watchman/watchman-subprocess.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python2
 
-# Copyright 2015-2018 Joel Allen Luellwitz, Andrew Klapp, and Brittney
-# Scaccia
+# Copyright 2015-2018 Joel Allen Luellwitz and Andrew Klapp
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
